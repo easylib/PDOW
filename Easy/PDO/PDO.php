5b02fd904dd197e5f749a6d382c797adbfa9efe7
kruemel <soeren@siegismund-poschmann.de>
2013-10-01 11:51:14 +0200

    PDO

---
+++ Easy/PDO/PDO.php
@@ -0,0 +1,74 @@
+<?php
+namespace Easy\PDO;
+class PDO
+{
+	private $db;
+	public function __construct($config)
+	{
+		$this->db = new PDO("mysql:host=".$config->get("host").";dbname=".$config->get("db")."", $config->get("user"), $config->get("pass")); 
+		$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		if($utf8)
+		{
+			$this->db->query("SET CHARACTER SET utf8;");
+			$this->db->query("SET NAMES utf8;");
+		}
+	}
+	/*
+	public function dbModel($db = NULL, $utf8 = true)
+	{
+		require '../config/mysql.config.php';
+		if($db!=NULL)
+		{
+			$mysql['db']= $db;
+		}
+		//$this->db = new mysqli($mysql["host"], $mysql["user"], $mysql["pass"], $mysql["db"]);
+		$this->db = new PDO("mysql:host=".$mysql['host'].";dbname=".$mysql['db']."", $mysql["user"], $mysql["pass"]); 
+	   $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	   if($utf8)
+	   {
+	   $this->db->query("SET CHARACTER SET utf8;");
+		$this->db->query("SET NAMES utf8;");
+	   }
+	}
+	*/
+	public function insert($statment, $data)
+	{
+		try { 
+		$STH = $this->db->prepare($statment); 
+		$STH->execute($data);
+		$STH->CloseCursor();
+		}
+		catch(PDOException $e) {
+			echo $e;  
+			}  
+	}
+	public function query($statment, $data)
+	{
+				try { 
+		$STH = $this->db->prepare($statment); 
+		$STH->execute($data);
+		$re = array();
+		while($row = $STH->fetch()) {
+			$re[] = $row; 
+		}
+		$STH->CloseCursor();
+		return $re;
+		}
+		catch(PDOException $e) {
+			echo $e;  
+			}  
+		//OLD
+		/*
+		$STH = $this->db->query($statment); 
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$re = array();
+		$STH->fetch($data);
+		while($row = $STH->fetch()) {
+			$re[] = $row;   
+		}    
+		return $re;
+		 * 
+		 */
+	}
+}
+?>
