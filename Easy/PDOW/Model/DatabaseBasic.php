5803b27b9abf1f92442b2f80c7ffa92533948de4
Soeren <soeren@siegismund-poschmann.de>
2015-02-09 11:55:58 +0100

    Create and Structur Check

--- Easy/PDOW/Model/DatabaseBasic.php
+++ Easy/PDOW/Model/DatabaseBasic.php
@@ -3,6 +3,7 @@ namespace Easy\PDOW\Model;
 
 class DatabaseBasic extends \Easy\PDOW\Model\Basic
 {
+	private $checkStructur = true; //Enabled or Dissabled the structur Check
 	public function __construct($name, $id = NULL)
 	{
 		$this->name = $name;
@@ -16,6 +17,7 @@ class DatabaseBasic extends \Easy\PDOW\Model\Basic
 				$this->data = $res[0];
 			}
 		}
+		$this->structurCheck = new \Easy\PDOW\Structur\Check($name, $this->db);
 	}
 	public function get($param, $data = false)
 	{
@@ -28,10 +30,98 @@ class DatabaseBasic extends \Easy\PDOW\Model\Basic
 	}
 	public function set($param, $value)
 	{
-		$this->data[$param] = $value;
-		$sql = 'UPDATE `'.$this->name.'` SET `'.$param.'`=? WHERE id = ?';
-		#var_dump($sql);
-		#var_dump(array($value, $this->get("id")));exit();
-		$res = $this->db->insert($sql, array($value, $this->get("id")));
+		if($this->checkStructur)
+		{
+			try
+			{
+				$check = $this->structurCheck->checkEntry($param, $value);
+				$check = true;
+			}
+			catch(\Exception $e)
+			{
+				$check = false;
+			}
+		}
+		else
+		{
+			$check = true;
+		}
+		if($check == true)
+		{
+			$this->data[$param] = $value;
+			$sql = 'UPDATE `'.$this->name.'` SET `'.$param.'`=? WHERE id = ?';
+			$res = $this->db->insert($sql, array($value, $this->get("id")));
+		}
+	}
+	public function create()
+	{
+		if($this->checkStructur)
+		{
+			try
+			{
+				$this->structurCheck->checkObject($this->data);
+				$check = true;
+			}
+			catch(\Exception $e)
+			{
+				$check = false;
+			}
+		}
+		else
+		{
+			$check = true;
+		}
+		if($check)
+		{
+			$structur = $this->getStuctur();
+			$sql = "INSERT INTO `".$this->name."` (";
+			$first = true;
+			foreach($structur as $name => $entry)
+			{
+				if(!$first)
+				{
+					$sql .= ', ';
+				}
+				$sql .= '`'.$name.'`';
+				$first = false;
+			}
+			$sql.= ") VALUES (";
+			$first = true;
+			foreach($structur as $name => $entry)
+			{
+				if(!$first)
+				{
+					$sql .= ', ';
+				}
+				$sql .= '?';
+				$first = false;
+			}
+			$sql.= ")";
+			#Create Value Array
+			$params = array();
+			foreach($structur as $name => $entry)
+			{
+				$data = NULL;
+				if(isset($this->data[$name]))
+				{
+					$data = $this->data[$name];
+				}
+				$params[] = $data;
+			}
+			$id = $this->db->insertID($sql, $params);
+			$this->__construct($this->name, $id);
+		}
+	}
+	private function getStuctur()
+	{
+		$sql = 'DESCRIBE ?';
+		$res = $this->db->query($sql, array($this->name);
+		$re = array();
+		foreach($res as $r)
+		{
+			$re[$r["Field"]] = array("Type"=>$r["Type"], "Null">$r["Null"], "Key"=>$r["Key"], "Default"=>$r["Default"], "Extra"=>$r["Extra"]);
+
+		}
+		return $re;
 	}
 }
---
+++ Easy/PDOW/Structur/Check.php
@@ -0,0 +1,96 @@
+<?php
+namespace Easy\PDOW\Structur;
+class Check extends \Easy\PDOW\Model\Basic
+{
+	function __construct($name, $db = NULL)
+	{
+		if($db != NULL)
+		{
+			$this->setDB($db);
+		}
+		$this->checkDB();
+		$this->getStuctur();
+	}
+	private function getStuctur($name)
+	{
+		$sql = 'DESCRIBE ?';
+		$res = $this->db->query($sql, array($name));
+		$re = array();
+		foreach($res as $r)
+		{
+			$re[$r["Field"]] = array("Type"=>$this->phareseDBTyp($r["Type"]), "Null">$r["Null"], "Key"=>$r["Key"], "Default"=>$r["Default"], "Extra"=>$r["Extra"]);
+
+		}
+		$this->data = $re;
+	}
+	private function phareseDBTyp($typ)
+	{
+		$orginal = $typ;
+		$start = strpos($typ, "(");
+		if($start>0)
+		{
+			$typ = substr($typ, 0, $start);
+			$limit = substr($typ, $start+1);
+			$ende = strpos($limit, ")");
+			$limit = substr($limit, 0, $ende);
+			return array("Typ"=>$typ, "Orginal"=>$orginal, "Limit"=>$limit);
+		}
+		else
+		{
+			return array("Typ"=>$typ, "Orginal"=>$orginal, "Limit"=>$limit=>NULL);
+		}
+
+
+	}
+	public function checkEntry($name, $value)
+	{
+		if(!isset($this->data[$name]))
+		{
+			throw new \Exception("Entry not Found", 1);
+		}
+		$context = $this->data[$name];
+		switch ($this->data[$name]["Typ"]["Typ"]) {
+			case 'int':
+				if(!is_numeric($value))
+				{
+					throw new \Exception("Int musst be Numeric", 1);
+				}
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+		if(isset($this->data[$name]["Typ"]["Limit"]))
+		{
+			if(strlen($value) > $this->data[$name]["Typ"]["Limit"])
+			{
+				throw new \Exception("Value is too long.", 1);
+			}
+		}
+		return true;
+	}
+	public function checkObject($data)
+	{
+		foreach($this->data as $name => $entry)
+		{
+			if(isset($data[$name]))
+			{
+				$this->checkEntry($name, $data[$name]);
+			}
+			else
+			{
+				if($entry["Null"]==false && $entry["Default"] ==NULL && $entry["Extra"]!="auto_increment")
+				{
+					throw new \Exception("Entry ".$name." is not set", 1);
+				}
+			}
+			unset($data[$name]);
+		}
+		if(count($data)>0)
+		{
+			throw new \Exception("Data set which are not in the Database", 1);
+			
+		}
+	}
+}
